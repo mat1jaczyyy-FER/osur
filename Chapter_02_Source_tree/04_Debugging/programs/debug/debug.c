fe7b0e49120de2477e0c0322d634ed6b3c54b734
Dominik Matijaca <dominik.matijaca@fer.hr>
2023-04-05 00:15:52 +0200

    1. Priprema koda "za učitavanje u ROM" i "izvođenje iz RAM-a"
    Nezgodno ovo s AT(lma)... mozda postoji bolji nacin, ali definitivno funkcionira ovako

--- Chapter_02_Source_tree/04_Debugging/programs/debug/debug.c
+++ Chapter_02_Source_tree/04_Debugging/programs/debug/debug.c
@@ -11,14 +11,13 @@ static int inc(int n)
 
 	return n;
 }
+int x = 5;
+int y[100];
+const int d = 123;
 
 int debug()
 {
 	int a, b, c;
-
-	printf("Example program: [%s:%s]\n%s\n\n", __FILE__, __FUNCTION__,
-		 debug_PROG_HELP);
-
 	a = 1;
 
 	b = a + 1;
@@ -29,16 +28,25 @@ int debug()
 	b += a + c;
 	c += a + b;
 
-	printf("a=%d, b=%d, c=%d\n", a, b, c);
-
 #if 1	/* compile with 'debug=yes' and without */
-	LOG(WARN, "This is log entry with WARN relevance");
-	LOG(INFO, "Address of 'a' is %x", &a);
+	void k_startup();
+	extern char system_info[];
+	extern void premjesti();
 
-	ASSERT_ERRNO_AND_RETURN(TRUE, EINVAL);
+	LOG ( INFO, "Address of premjesti (loader) is     %x", premjesti );
+	LOG ( INFO, "Address of k_startup (krn code) is   %x", k_startup );
+	LOG ( INFO, "Address of system_info (krn data) is %x", system_info );
+	LOG ( INFO, "Address of 'debug' (prog code) is    %x", debug );
 
-	ASSERT(TRUE);
-	//ASSERT(FALSE);
+	LOG ( INFO, "Address of 'a' (stack variable) is   %x", &a );
+	LOG ( INFO, "Address of 'd' (read-only const) is  %x", &d );
+	LOG ( INFO, "Address of 'x' (global variable) is  %x", &x );
+	//LOG ( INFO, "Address of 'y' is %x", y );
+	
+	extern int xxxx;
+	LOG ( INFO, "  Value of 'xxxx' is %x", xxxx );
+	LOG ( INFO, "Address of 'xxxx' is %x", &xxxx );
+
 #endif
 	return 0;
 }
