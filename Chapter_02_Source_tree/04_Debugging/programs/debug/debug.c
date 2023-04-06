5b422f163c260ad519b3eabc5d1cb5d906b70513
Dominik Matijaca <dominik.matijaca@fer.hr>
2023-04-06 13:19:19 +0200

    Lab2 uzivo

--- Chapter_02_Source_tree/04_Debugging/programs/debug/debug.c
+++ Chapter_02_Source_tree/04_Debugging/programs/debug/debug.c
@@ -31,9 +31,9 @@ int debug()
 #if 1	/* compile with 'debug=yes' and without */
 	void k_startup();
 	extern char system_info[];
-	extern void premjesti();
+	extern void arch_startup();
 
-	LOG ( INFO, "Address of premjesti (loader) is     %x", premjesti );
+	LOG ( INFO, "Address of arch_startup (boot) is    %x", arch_startup );
 	LOG ( INFO, "Address of k_startup (krn code) is   %x", k_startup );
 	LOG ( INFO, "Address of system_info (krn data) is %x", system_info );
 	LOG ( INFO, "Address of 'debug' (prog code) is    %x", debug );
@@ -41,11 +41,6 @@ int debug()
 	LOG ( INFO, "Address of 'a' (stack variable) is   %x", &a );
 	LOG ( INFO, "Address of 'd' (read-only const) is  %x", &d );
 	LOG ( INFO, "Address of 'x' (global variable) is  %x", &x );
-	//LOG ( INFO, "Address of 'y' is %x", y );
-	
-	extern int xxxx;
-	LOG ( INFO, "  Value of 'xxxx' is %x", xxxx );
-	LOG ( INFO, "Address of 'xxxx' is %x", &xxxx );
 
 #endif
 	return 0;
